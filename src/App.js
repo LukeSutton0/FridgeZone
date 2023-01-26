4d0aa784c80bab2b523d84ffc1a0cd977dc8b831
Luke <luke.sutton.work@gmail.com>
2023-01-26 12:52:19 +0000

    Started intital react parts
    Got a base react system going

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from "./header"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Test</h1>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./header.css";
+
+function Header(){
+    return(
+        <div className="header">
+            <h2>FridgeZone</h2>
+             </div>
+    );
+}
+export default Header;
