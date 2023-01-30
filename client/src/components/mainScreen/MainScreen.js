f6f1f912fc2a6e32d37ef2f580b3755a48e628e3
Luke <luke.sutton.work@gmail.com>
2023-01-30 15:47:50 +0000

    Bootstrap
    bootstrap support should be implemented now, not sure exactly how it works but the packages and imports are there

--- client/src/components/mainScreen/MainScreen.js
+++ client/src/components/mainScreen/MainScreen.js
@@ -1,8 +1,11 @@
 import React from "react";
 import classes from './MainScreen.module.css'
+import globalStyles from '../../Assets/global-styles/bootstrap.min.module.css'
+
 const SignUp = () =>{
     return(
         <div className={classes.inputBox}>
+          <button className={globalStyles.btn,globalStyles.btn-primary}> Bootstrap Button </button>
           <p>Login</p>
           <p>Sign Up</p>
         </div>
