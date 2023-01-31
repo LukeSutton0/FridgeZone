f6261268ba3f6a60b2975e9e673aed3f2c5acf25
Luke <80263461+LukeSutton0@users.noreply.github.com>
2023-01-31 22:52:05 +0000

    Form loading from react
    Form loading sign up form from sign up form js

--- client/src/components/signUp/SignUpForm.js
+++ client/src/components/signUp/SignUpForm.js
@@ -1,32 +1,29 @@
 import { render } from "@testing-library/react";
-import React from "react";
+import React, { Fragment } from "react";
 import classes from './SignUp.module.css'
 
 class SignUpForm extends React.Component{
 
-
-
-
     render(){
-        return(
-            <form>
-                <label for="UserName">UserName
-                <input type="text" id="UserName" name="UserName" value="?"/>
-                </label>
-                <label for="Password">Password
-                <input type="text" id="Password" name="Password" value="?"/>
-                </label>
-                <label for="Job Title">Job Title
-                <input type="select" id="UserName" name="UserName" value="?"/>
-                </label>
-                <label for="FullName">FullName
-                <input type="text" id="FullName" name="FullName" value="?"/>
-                </label>
-                <input type="submit" value="Submit"/>
+        return (
+            <Fragment>
+            <form>,
+                <label for="UserName">UserName,
+                <input type="text" id="UserName" name="UserName" value="?"/>,
+                </label>,
+                <label for="Password">Password,
+                <input type="text" id="Password" name="Password" value="?"/>,
+                </label>,
+                <label for="Job Title">Job Title,
+                <input type="select" id="UserName" name="UserName" value="?"/>,
+                </label>,
+                <label for="FullName">FullName,
+                <input type="text" id="FullName" name="FullName" value="?"/>,
+                </label>,
+                <input type="submit" value="Submit"/>,
             </form>
-            <h1>Sign Up</h1>
-            <p>para</p>
-        );
+            </Fragment>
+        )
     }
 }
 
