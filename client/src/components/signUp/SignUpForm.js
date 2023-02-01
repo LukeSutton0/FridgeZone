e35865e8ae88e825b6131cdb43dc54b184681daa
Luke <luke.sutton.work@gmail.com>
2023-02-01 12:05:13 +0000

    Form working with react
    React form working, needs http endpoint for submission, needs styling

--- client/src/components/signUp/SignUpForm.js
+++ client/src/components/signUp/SignUpForm.js
@@ -1,30 +1,81 @@
-import { render } from "@testing-library/react";
-import React, { Fragment } from "react";
+//import { render } from "@testing-library/react";
+import React, { Fragment} from "react";
+import { useState } from "react";
+import { scryRenderedComponentsWithType } from "react-dom/test-utils";
+
 import classes from './SignUp.module.css'
 
-class SignUpForm extends React.Component{
+const SignUpForm = () => {
 
-    render(){
-        return (
-            <Fragment>
-            <form>,
-                <label for="UserName">UserName,
-                <input type="text" id="UserName" name="UserName" value="?"/>,
-                </label>,
-                <label for="Password">Password,
-                <input type="text" id="Password" name="Password" value="?"/>,
-                </label>,
-                <label for="Job Title">Job Title,
-                <input type="select" id="UserName" name="UserName" value="?"/>,
-                </label>,
-                <label for="FullName">FullName,
-                <input type="text" id="FullName" name="FullName" value="?"/>,
-                </label>,
-                <input type="submit" value="Submit"/>,
-            </form>
-            </Fragment>
-        )
+    const [UserName, setUserName] = useState('');
+    const [Password, setPassword] = useState('');
+    const [JobTitle, setJobTitle] = useState('');
+    const [FullName, setFullName] = useState('');
+
+    const handleSubmit = (e) =>{
+        e.preventDefault() //stops page refreshing
+        const data = {UserName,Password,JobTitle,FullName}
+        console.log(data);
+        //error checking client side
+        // then send http
+        sendPost(data)
     }
+
+
+    const sendPost= (data) =>{
+        fetch('http://localhost:3000',{
+            method: "POST",
+            headers:{
+                'Accept':'application/json',
+                'Content-Type': 'application/json',
+            },
+            credentials: "omit",
+            body: JSON.stringify(data)
+        })
+    }
+
+
+
+    return (
+        <Fragment>
+            <form onSubmit={handleSubmit}>
+                <label>Username:</label>
+                <input 
+                    type="text"
+                    required
+                    value={UserName}
+                    placeholder = "Johndoe123"
+                    onChange={(e) => setUserName(e.target.value)}/>
+                <label>Password:</label>
+                <input 
+                    type="text"
+                    required
+                    value={Password}
+                    placeholder = "Password123"
+                    onChange={(e) => setPassword(e.target.value)}/>
+                <select
+                value={JobTitle}
+                onChange={(e)=>setJobTitle(e.target.value)}>
+                    <option value="DeliveryDriver">DeliveryDriver</option>
+                    <option value="HeadChef">HeadChef</option>
+                    <option value="Chef">Chef</option>
+                    <option value="Admin">Admin</option>
+                    <option value="Supplier">Supplier</option>
+                    <option value="HealthAndSafetyOfficer">Health and Safety Officer</option>
+                </select>
+                <label>Full Name:</label>
+                <input 
+                    type="text"
+                    required
+                    value={FullName}
+                    placeholder = "Johnathon Doedecahedron"
+                    onChange={(e) => setFullName(e.target.value)}/>
+
+                <button>Submit</button>
+            </form>
+        </Fragment>
+    )
+
 }
 
 export default SignUpForm;
