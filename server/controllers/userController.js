f2b059767510dbd95f53a7663d82d0f65923df40
Luke <80263461+LukeSutton0@users.noreply.github.com>
2023-02-02 18:55:50 +0000

    Login and Signup Tokens
    Login and signup tokens created, postman works with logging in. Looking at react now

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,23 @@
 const User = require('../models/userModel.js')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    return jwt.sign({_id:_id},process.env.SECRET,{expiresIn:'2d'})
+}
+
 
 //login
 const loginUser = async(req,res)=>{
-    res.json({mssg:'Login user'})
+    const {username,password} = req.body
+
+    try{
+        const user = await User.login(username,password)
+        const token = createToken(user._id)
+        res.status(200).json({username,token})
+    }
+    catch(error){
+        res.status(400).json({error:error.message})
+    }
 }
 
 //signUp
@@ -11,7 +26,12 @@ const signUpUser = async(req,res)=>{
     const{username,password,jobtype,fullname} = req.body
     try{
         const user = await User.signUp(username,password,jobtype,fullname)
-        res.status(200).json({username,user})
+
+        //create token
+        const token = createToken(user._id)
+
+
+        res.status(200).json({username,token})
     }
     catch(error){
         res.status(400).json({error:error.message})
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const Schema = mongoose.Schema
-
+const validator = require('validator')
 const userSchema = new Schema({
     username:{
         type: String,
@@ -26,12 +26,24 @@ const userSchema = new Schema({
 //static signup method
 
 userSchema.statics.signUp = async function(username,password,jobtype,fullname) {
+    //validation
+    if(!username || !password || !jobtype || !fullname){
+        throw Error ('All fields must be filled')
+    }
+    if (!validator.isStrongPassword(password,{minLength:4})){
+        
+        throw Error ('Password not strong enough')
+    }
+//Check if a password is strong or not. Allows for custom requirements or scoring rules. 
+//If returnScore is true, then the function returns an integer score for the password rather than a boolean.
+// Default options:
+// { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1, 
+//returnScore: false, pointsPerUnique: 1, pointsPerRepeat: 0.5, pointsForContainingLower: 10,
+// pointsForContainingUpper: 10, pointsForContainingNumber: 10, pointsForContainingSymbol: 10 }
     const exists =  await this.findOne({username})
-
     if (exists){
         throw Error("Username already in use")
     }
-
     //password hash
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password,salt)
@@ -44,5 +56,24 @@ userSchema.statics.signUp = async function(username,password,jobtype,fullname) {
     })
     return user
 }
+
+userSchema.statics.login = async function(username,password){
+    if(!username || !password){
+        throw Error ('Enter email and password')
+    }
+    const user =  await this.findOne({username})
+    if (!user){
+        throw Error("Username not found")
+    }
+    const match = await bcrypt.compare(password,user.password)
+
+    if(!match){
+        throw Error("Incorrect Password")
+    }
+
+    return user
+}
+
+
 
 module.exports = mongoose.model('User',userSchema)
