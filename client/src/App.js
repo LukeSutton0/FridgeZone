6a9d0e33f4b1adc8520b86da11cbf223a234e4a9
Luke <80263461+LukeSutton0@users.noreply.github.com>
2023-02-02 19:43:15 +0000

    Changing file format
    moved into pages folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import './App.css';
 
 //module components
 //{MainHeader}
 import MainHeader from './components/header/MainHeader.js'
 import MainFooter from './components/footer/Footer.js'
-import MainScreen from './components/mainScreen/MainScreen.js'
-import ErrorPage  from './components/error/ErrorPage.js'
+import MainScreen from './pages/mainScreen/MainScreen.js'
+import ErrorContent  from './components/error/ErrorContent.js'
 import { BrowserRouter as Router,Routes,Route, Link } from 'react-router-dom';
 
-import SignUp from './components/signUp/SignUp.js'
+import SignUp from './pages/signUp/SignUp.js'
 
-import Login from './components/login/Login.js'
+import Login from './pages/login/Login.js'
 
 function App() {
   return (
     <Router>
       <div className="App">
         <MainHeader/>
-
         <div className="content">
           <Routes>
             <Route path="/" element={<MainScreen/>}>
+            </Route>
+            <Route path="*" element={<ErrorContent/>}>
             </Route>
             <Route path="/signUp" element={<SignUp/>}>
             </Route>
