0c7efb94a144cd56c954efba7fc70a43b6544189
Muhammad-786 <Shaheervipiteno786@gmail.com>
2023-02-06 15:01:31 +0000

    Added search and reset option in details
    The user is now able to search items on the details page using the search bar

--- client/src/pages/details/Details.js
+++ client/src/pages/details/Details.js
@@ -1,37 +1,65 @@
 import React, { useEffect, useState } from "react";
 import StockDetails from "./StockDetails";
 
-
-
-const Details = () => {  
-  const [stock, setStock] = useState(null)
+const Details = () => {
+  const [stock, setStock] = useState(null);
+  const [originalStock, setOriginalStock] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    const fetchstock = async () => {
-      const response = await fetch('http://localhost:4000/stock')
-      const json = await response.json()
+    const fetchStock = async () => {
+      const response = await fetch("http://localhost:4000/stock");
+      const json = await response.json();
 
       if (response.ok) {
-        setStock(json)
+        setStock(json);
+        setOriginalStock(json);
+      }
+    };
 
-    }
-  }
-    fetchstock();
+    fetchStock();
+  }, []);
 
-  }, [])
-  
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearch = () => {
+    setStock(
+      originalStock.filter((item) => {
+        return (
+          item._id === searchTerm ||
+          item.quantity == searchTerm ||
+          item.expiryDate === searchTerm ||
+          item.supplier === searchTerm
+        );
+      })
+    );
+  };
+
+  const handleReset = () => {
+    setStock(originalStock);
+  };
 
   return (
     <div className="stockhome">
       <div className="stocks">
-      <h1>Stock Details</h1>
-      {stock && stock.map((stock) => (
-        <StockDetails key={stock.id} stock={stock} />
-      ))}
+        <h1>Stock Details</h1>
+        <div className="search-container">
+          <input type="text" value={searchTerm} onChange={handleChange} />
+          <button onClick={handleSearch}>Search</button>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+        {stock && stock.length > 0 ? (
+          stock.map((stock) => (
+            <StockDetails key={stock.id} stock={stock} />
+          ))
+        ) : (
+          <p>No matching items found</p>
+        )}
       </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default Details;
