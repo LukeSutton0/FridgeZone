00f50f7ec008a6d441830e9d75628b8e20b8ddf8
Luke <luke.sutton.work@gmail.com>
2023-02-10 09:53:56 +0000

    Admin Utilities complete

--- client/src/components/adminDash/AdminDashRemoveMap.js
+++ client/src/components/adminDash/AdminDashRemoveMap.js
@@ -2,36 +2,34 @@ import classes from './AdminDash.module.css'
 import React, { Fragment, useState } from "react";
 import { useAuthContext } from "../../hooks/useAuthContext";
 
-const ChangeUserPass = ({userPassChange}) => {
+const RemoveUser = ({userRemove}) => {
     const {user} = useAuthContext()
     const storecode = user.storecode
-    const _id = userPassChange._id
-    const [newPass,setNewPass] = useState('')
+    const _id = userRemove._id
+    //const [newRole,setNewRole]= useState('')
     const [message, setMessage] = useState('')
     const [error, setError] = useState(null)
 
     const handleSubmit = async (e) =>{
         e.preventDefault()
         try{
-            const response = await fetch('http://localhost:4000/user/changepassword', {
-                method: 'PATCH',
+            const response = await fetch('http://localhost:4000/user/delete', {
+                method: 'POST',
                 headers: {
                     'Accept':'application/json',
                     'Content-Type': 'application/json',
                     'Authorisation':`Bearer ${user.token}`
                 },
-                body: JSON.stringify({storecode,newPass,_id})
+                body: JSON.stringify({storecode,_id})
             })
             const json = await response.json()
             if (!response.ok) {
                 setMessage('')
-                console.log(json)
-                setError('Password requires minimum 4 characters: 1 lowercase, 1 uppercase 1 symbol, 1 number')
+                setError('User not found')
             }
             if (response.ok) {
-                setMessage(`Changed Successfully`)
+                setMessage(`User Removed Successfully`)
                 setError(null)
-                //console.log('New stock added:', json)
             }
         }
         catch(error){
@@ -42,19 +40,13 @@ const ChangeUserPass = ({userPassChange}) => {
         <Fragment>
             <form onSubmit={handleSubmit} className={classes.adminDashIndividual}>
                 <div>
-                    <p>{userPassChange.username}</p>
+                    <p>{userRemove.username}</p>
                 </div>
                 <div>
-                    <p>{userPassChange._id}</p>
+                    <p>{userRemove._id}</p>
                 </div>
                 <div className={classes.userPassChangeDiv}>
-                    <input 
-                        type="text"
-                        required
-                        value={newPass}
-                        placeholder = "NewPass1#"
-                        onChange={(e) => setNewPass(e.target.value)}/>
-                    <button>Submit</button>
+                    <button>Remove User</button>
                     {error &&(
                         <div>{error}</div>
                     )}
@@ -69,4 +61,4 @@ const ChangeUserPass = ({userPassChange}) => {
 }
 
 
-export default ChangeUserPass;
+export default RemoveUser;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { loginUser,signUpUser,viewUsers,changePass} = require('../controllers/userController')
+const { loginUser,signUpUser,viewUsers,changePass,deleteUser,changeRole} = require('../controllers/userController')
 const router = express.Router()
 
 //login route
@@ -10,9 +10,16 @@ router.post('/view',viewUsers)
 
 router.patch('/changepassword',changePass)
 
+//change users role/title
+router.patch('/changerole',changeRole)
 //signup route
 router.post('/signup',signUpUser)
 router.get('/signup', async(req,res)=>{
 })
+
+//delete user
+
+router.post('/delete',deleteUser)
+
 
 module.exports = router
